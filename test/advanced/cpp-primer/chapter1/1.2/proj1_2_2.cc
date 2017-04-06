281cb7313b584bd7e461a46cbad58bf2095ba330
Haibo Hao <haohaibo031113@163.com>
2017-04-06 21:10:41 +0800

    done 1.2.2

---
+++ test/advanced/cpp-primer/chapter1/1.2/proj1_2_2.cc
@@ -0,0 +1,16 @@
+/*
+ * Author: Haibo Hao
+ * Date  : 2017.4.6
+ * Email : haohaibo@ncic.ac.cn
+ * */
+
+#include <iostream>
+int main()
+{
+    std::cout << "Enter two numbers: " << std::endl;
+    int v1, v2;
+    std::cin >> v1 >> v2;
+    std::cout << "The sum of " << v1 << " and "
+              << v2 << " is " << v1 + v2 << std::endl;
+    return 0;
+}
