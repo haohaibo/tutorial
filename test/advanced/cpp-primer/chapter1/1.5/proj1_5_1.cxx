378e153d84d6759bff3e105dea77f1a0857223ea
Haibo Hao <haohaibo031113@163.com>
2017-04-08 16:58:29 +0800

    chapter1 1.5.1 class Sales_item read and write

---
+++ test/advanced/cpp-primer/chapter1/1.5/proj1_5_1.cxx
@@ -0,0 +1,26 @@
+/*
+*      Filename: proj1_5_1.cxx
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-04-08 16:35:38
+* Last Modified: 2017-04-08 16:35:38
+**/
+#include <iostream>
+#include "Sales_item.h"
+/* chapter1 1.5小节 类的简介 
+ *  C++设计的主要焦点就是使所定义的类类型(class type) 
+ *  的行为可以像内置类型一样自然
+ */
+int main()
+{
+    Sales_item book;
+    // read ISBN, number of copies sold, and sales price
+    std::cin >> book;
+
+    // write ISBN, number of copies sold, total revenue, and sales price
+    std::cout << book << std::endl;
+    return 0;
+}
+
