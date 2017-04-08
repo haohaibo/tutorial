2af6ae0fbce21b0c6a5edcdf5a6c102ad797d2cc
Haibo Hao <haohaibo031113@163.com>
2017-04-08 17:14:17 +0800

    chapter1 use class Sales_item add

---
+++ test/advanced/cpp-primer/chapter1/1.5/proj1_5_2.cxx
@@ -0,0 +1,20 @@
+/*
+*
+*      Filename: proj1_5_2.cxx
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-04-08 16:59:19
+* Last Modified: 2017-04-08 16:59:19
+**/
+#include <iostream>
+#include "Sales_item.h"
+int main()
+{
+    Sales_item item1, item2;
+    std::cin >> item1 >> item2; // read a pair of transactions
+    std::cout << item1 + item2 << std::endl; // print their sum
+    return 0;
+}
+
