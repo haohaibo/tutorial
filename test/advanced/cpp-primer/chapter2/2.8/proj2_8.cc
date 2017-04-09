c6a3ff155175b5adc497dad469971da9604cdcb3
Haibo Hao <haohaibo031113@163.com>
2017-04-09 23:18:55 +0800

    chapter2 2.8 class type

---
+++ test/advanced/cpp-primer/chapter2/2.8/proj2_8.cc
@@ -0,0 +1,37 @@
+/*
+*
+*      Filename: proj2_8.cc
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-04-09 20:50:03
+* Last Modified: 2017-04-09 20:50:03
+**/
+#include <iostream>
+#include <string>
+class Sales_item
+{
+    public:
+        // operations on Sales_item objects will go here
+    private:
+        std::string isbn;
+        unsigned int units_sold;
+        double revenue;
+};
+
+/*struct Sales_item
+{
+    // operations on Sales_item objects
+    private:
+        std::string isbn;
+        unsigned int units_sold;
+        double revenue;
+};
+*/
+
+int main()
+{
+    
+}
+
