d96086a9fb2aa8633ded9761f0e7b6bc2cff9c0e
Haibo Hao <haohaibo031113@163.com>
2017-04-09 10:17:16 +0800

    mulit-line string literal

---
+++ test/advanced/cpp-primer/chapter2/proj2_2_6.cpp
@@ -0,0 +1,29 @@
+/*
+*
+*      Filename: proj2_2_6.cpp
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-04-08 22:16:41
+* Last Modified: 2017-04-08 22:51:35
+**/
+#include <iostream>
+
+int main()
+{
+    std::cout << "a multi-line "
+                 "string literal "
+                 "using concatenation"
+              << std::endl;
+    std::cou\
+t << "hi" << std::endl;
+
+    // multiline string literal
+    std::cout << "a multi-line \
+string literal \
+using a backslash"
+              << std::endl;
+    return 0;
+}
+
