0981b77fed25caef6f743e61ef894a0d49bc8620
Haibo Hao <haohaibo031113@163.com>
2017-04-10 16:15:26 +0800

    chapter2 2.9 use header file

---
+++ test/advanced/cpp-primer/chapter2/2.9/Sales_item.cc
@@ -0,0 +1,23 @@
+/*
+*
+*      Filename: Sales_item.cc
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-04-10 00:42:18
+* Last Modified: 2017-04-10 00:42:18
+**/
+#ifndef SALESITEM_H
+#define SALESITEM_H
+#include <iostream>
+class Sales_item
+{
+    public:
+        // some functions
+    private:
+        std::string isbn;
+        unsigned int units_sold;
+        double revenue;
+};
+#endif
---
+++ test/advanced/cpp-primer/chapter2/2.9/main.cc
@@ -0,0 +1,19 @@
+/*
+*
+*      Filename: main.cc
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-04-10 00:46:19
+* Last Modified: 2017-04-10 00:46:19
+**/
+#include <iostream>
+#include "Sales_item.cc"
+
+int main()
+{
+    Sales_item sales;
+    return 0;
+}
+
