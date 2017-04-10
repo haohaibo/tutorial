889a2ad5593991a6b9d20e9a4090e03d843286b1
Haibo Hao <haohaibo031113@163.com>
2017-04-10 16:35:08 +0800

    chapter3 3.1 using

---
+++ test/advanced/cpp-primer/chapter3/3.1/proj3_1.cc
@@ -0,0 +1,24 @@
+/*
+*
+*      Filename: proj3_1.cc
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-04-10 16:16:57
+* Last Modified: 2017-04-10 16:16:57
+**/
+#include <iostream>
+#include <string>
+
+// using namespace::name;
+using std::cin;
+using std::string;
+int main()
+{
+    string s;
+    cin >> s;
+    std::cout << s;
+    return 0;
+}
+
---
+++ test/advanced/cpp-primer/chapter3/3.1/proj3_2.cc
@@ -0,0 +1,26 @@
+/*
+*
+*      Filename: proj3_2.cc
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-04-10 16:28:26
+* Last Modified: 2017-04-10 16:28:26
+**/
+#include <iostream>
+
+// using declarations for names from the standard library
+using std::cin;
+using std::cout;
+using std::endl;
+int main()
+{
+    cout << "Enter two numbers:" << endl;
+    int v1, v2;
+    cin >> v1 >> v2;
+    cout << "The sum of " << v1
+         << " and " << v2
+         << " is " << v1 + v2 << endl;
+    return 0;
+}
