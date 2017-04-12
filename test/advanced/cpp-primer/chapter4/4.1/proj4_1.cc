930ad42ba8761b13b22d946de3a76710032d9ecd
Haibo Hao <haohaibo031113@163.com>
2017-04-12 11:30:48 +0800

    chapter4 4.1 array

---
+++ test/advanced/cpp-primer/chapter4/4.1/proj4_1.cc
@@ -0,0 +1,38 @@
+/*
+*
+*      Filename: proj4_1.cc
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-04-12 09:57:47
+* Last Modified: 2017-04-12 09:57:47
+**/
+#include <iostream>
+#include <string>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+    // both buf_size and max_files are const
+    const unsigned buf_size = 512, max_files = 20;
+    int staff_size = 28; 
+    char input_buffer[buf_size];
+    string fileTable[max_files + 1];
+
+    double salaries[staff_size];
+
+    const unsigned array_size = 3;
+    int ia[array_size] = {0, 1, 2};
+    string str_array[array_size] = {"hi", "bye"};
+    cout << str_array[0] << endl;
+
+    char ca1[] = {'C', '+', '+'};  // no null
+    char ca2[] = {'C', '+', '+', '\0'}; // explicit null
+    char ca3[] = "C++"; // null terminator added automatically
+    return 0;
+}
+
---
+++ test/advanced/cpp-primer/chapter4/4.1/proj4_1_2.cc
@@ -0,0 +1,33 @@
+/*
+*
+*      Filename: proj4_1_2.cc
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-04-12 10:51:37
+* Last Modified: 2017-04-12 10:51:37
+**/
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    const size_t array_size = 10;
+    int ia[array_size];
+
+    for(size_t index = 0; index != array_size; ++index)
+    {
+        ia[index] = index;
+    }
+
+    for(size_t index = 0; index != array_size; ++index)
+    {
+       cout <<  ia[index] << "\t";
+    }
+    cout << endl;
+    return 0;
+}
+
