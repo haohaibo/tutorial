c3f01a814bd538b26db744a9ac810a4064667248
Haibo Hao <haohaibo031113@163.com>
2017-04-18 12:12:09 +0800

    string.h strchr

---
+++ test/basic/string_h/strchr.cc
@@ -0,0 +1,25 @@
+/*
+*
+*      Filename: strchr.cc
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-04-18 11:20:22
+* Last Modified: 2017-04-18 11:20:22
+**/
+#include <string.h>
+#include <stdio.h>
+
+int main()
+{
+    char string[] = {"This is a string"};
+    char *ptr, c = 'r';
+    ptr = strchr(string, c);
+    if(ptr)
+        printf("The character %c is at position: %s\n", c, ptr);
+    else
+        printf("The character was not found\n");
+    return 0;
+}
+
