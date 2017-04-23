920275854175ba96b92213e49bc142215497be53
Haibo Hao <haohaibo031113@163.com>
2017-04-23 18:50:47 +0800

    leetcode 561

---
+++ leetcode/array_partition_1_561.cpp
@@ -0,0 +1,41 @@
+/*
+ *
+ *      Filename: array_partition_1_561.cpp
+ *
+ *        Author: Haibo Hao
+ *        Email : haohaibo@ncic.ac.cn
+ *   Description: ---
+ *        Create: 2017-04-23 18:36:37
+* Last Modified: 2017-04-23 18:46:03
+ **/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+    public:
+        int arrayPairSum(vector<int>& nums) {
+            sort(nums.begin(), nums.end());
+            int sum = 0;
+            for(int i=0; i < nums.size(); i+=2)
+            {
+                sum += nums[i];
+            }
+            return sum;
+        }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> data;
+    data.push_back(1);
+    data.push_back(4);
+    data.push_back(2);
+    data.push_back(3);
+    cout << sol.arrayPairSum(data) << endl;
+    return 0;
+}
+
