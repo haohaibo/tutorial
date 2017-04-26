d1a2ec7b97a73de08fe3a2a4f70c21b6bddb6c87
Haibo Hao <haohaibo031113@163.com>
2017-04-26 10:33:27 +0800

    PAT group tianqi L1-003

---
+++ PAT/group_tianti/L1-003.cpp
@@ -0,0 +1,44 @@
+/*
+*
+*      Filename: L1-003.cpp
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-04-26 09:19:30
+* Last Modified: 2017-04-26 09:19:30
+**/
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <string.h>
+
+
+using namespace std;
+
+int main()
+{
+    string str;
+    int out[10];
+    memset(out, 0, sizeof(out));
+    cin >> str;
+    char ch;
+    int index;
+    for(int i = 0; i < str.size(); ++i)
+    {
+       ch = str[i]; 
+        index = (int)(ch - '0');
+       // cout << index << endl;
+        out[index]++; 
+    }
+    for(int i=0; i < 10; ++i)
+    {
+        if(out[i] > 0)
+        {
+            printf("%d:%d\n",i,out[i]);
+        }
+
+    }
+    return 0;
+}
+
