af53b4bbc04a4bcb9e303cf290540aa0fd9261a6
HenryHao <haohaiboyongyuan@gmail.com>
2017-04-27 09:24:30 +0800

    PAT L1-010ing

---
+++ PAT/group_tianti/L1-010.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int data[3];
+    scanf("%d",&data[0]);
+    scanf("%d",&data[1]);
+    scanf("%d",&data[2]);
+    for(int i = 0; i < 2; ++i)
+    {
+        if(data[i] < data[i+1])
+        {
+            int temp;
+            
+        }
+    }
+}
