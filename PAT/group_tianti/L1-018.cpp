e5b98259e826efffa87e20461a5402cc4af4720d
Haibo Hao <haohaibo031113@163.com>
2017-04-28 01:04:25 +0800

    PAT L1-018

---
+++ PAT/group_tianti/L1-018.cpp
@@ -0,0 +1,40 @@
+/*
+*
+*      Filename: L1-018.cpp
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-04-28 00:37:58
+* Last Modified: 2017-04-28 00:37:58
+**/
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    string time;
+    cin >> time;
+    int hh,mm;
+    hh = (time[0] - '0')*10 + (time[1] - '0');
+    mm = (time[3] - '0')*10 + (time[4] - '0');
+    if((hh >= 0 && hh <= 11)||(hh == 12 && mm == 0))
+    {
+       cout << "Only " << time << ".  Too early to Dang." << endl;
+    }else
+    {
+       int count = 0;
+       count = hh - 12; 
+       if(mm > 0)
+       {
+           count++;
+       }
+       for(int i = 0; i < count; ++i)
+       {
+           printf("Dang");
+       }
+       printf("\n");
+    }
+    return 0;
+}
