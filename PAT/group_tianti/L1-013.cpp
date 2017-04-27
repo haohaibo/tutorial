a4fa540e545d3223b5e6f8fc2cbd34ee62eaba49
HenryHao <haohaiboyongyuan@gmail.com>
2017-04-27 13:19:23 +0800

    PAT L1-013

---
+++ PAT/group_tianti/L1-013.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+long fact(int n)
+{
+    int sum=1;
+    while( n >= 1)
+    {
+        sum *=n;
+        --n;
+    }
+    return sum;
+}
+
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    long sum = 0;
+    while( N >= 1)
+    {
+        sum += fact(N);
+        --N;
+    } 
+    printf("%ld\n",sum);
+    return 0;
+}
