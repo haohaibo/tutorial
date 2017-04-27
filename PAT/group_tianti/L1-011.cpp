006a45dba95822557738a85e94624ea8087ef09c
HenryHao <haohaiboyongyuan@gmail.com>
2017-04-27 12:49:28 +0800

    PAT L1-011

---
+++ PAT/group_tianti/L1-011.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string A, B;
+    getline(cin, A);
+    getline(cin, B);
+    //cout << A << endl;
+    //cout << B << endl;
+    for(int i = 0; i < A.size(); ++i)
+    {
+        if(B.find(A[i]) != string::npos)
+        {
+            A.erase(i,1);
+            --i;
+        }
+    }
+    cout << A << endl;
+    return 0;
+}
---
+++ PAT/group_tianti/test2.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>  
+#include <string.h>  
+int main()  
+{  
+    char s[10001],c;  
+    int a[256],i=0;  
+    memset(a,0,256*sizeof(int));  
+    gets(s);  
+    while( (c=getchar()) !='\n')  
+    {  
+        a[c] = 1;  
+    }  
+    while(s[i] != 0)  
+    {  
+        if(!a[s[i]])  
+        {  
+            putchar(s[i]);  
+        }  
+        i++;  
+    }  
+    putchar('\n');  
+    return 0;  
+} 
