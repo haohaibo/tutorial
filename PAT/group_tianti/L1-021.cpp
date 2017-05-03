dfa4103673c90b31a3df6b54df1dd4a3ccb373f1
Haibo Hao <haohaibo031113@163.com>
2017-05-04 01:08:00 +0800

    PAT L1-021

---
+++ PAT/group_tianti/L1-021.cpp
@@ -0,0 +1,21 @@
+/*
+*
+*      Filename: L1-021.cpp
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-05-04 00:21:56
+* Last Modified: 2017-05-04 00:35:04
+**/
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    printf("I'm gonna WIN!\n");
+    printf("I'm gonna WIN!\n");
+    printf("I'm gonna WIN!\n");
+    return 0;
+}
