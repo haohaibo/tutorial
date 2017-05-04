8e5b5ba8c7aea4d19ad8f6a926f27ef22d556ae4
Haibo Hao <haohaibo031113@163.com>
2017-05-04 17:00:48 +0800

    PAT L1-026

---
+++ PAT/group_tianti/L1-026.cpp
@@ -0,0 +1,25 @@
+/*
+*
+*      Filename: L1-026.cpp
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-05-04 16:50:05
+* Last Modified: 2017-05-04 17:00:11
+**/
+#include <iostream>
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string love = "I Love GPLT";
+    for(int i = 0; i < love.size(); ++i)
+    {
+        cout << love[i] << endl;
+    }
+    return 0;
+}
+
