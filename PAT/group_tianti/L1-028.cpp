1cfa7ddc95ec970f62ff1ec29462751328d2d1f6
Haibo Hao <haohaibo031113@163.com>
2017-05-06 01:21:31 +0800

    PAT L1-028 L1-029

---
+++ PAT/group_tianti/L1-028.cpp
@@ -0,0 +1,54 @@
+/*
+*
+*      Filename: L1-028.cpp
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-05-06 00:31:03
+* Last Modified: 2017-05-06 01:02:25
+**/
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    int num;
+    bool prime = true;
+    while(--N >= 0)
+    {
+        prime = true;
+        scanf("%d",&num);
+        if(num == 1)
+        {
+            printf("No\n");
+            continue;
+        }
+        if(num == 2)
+        {
+            printf("Yes\n");
+            continue;
+        }
+        for(int i = 2; i <= sqrt(num); ++i)
+        {
+           //printf("%d",num%i);
+           if((num%i) == 0)
+           {
+               prime = false;
+               break;
+           }
+        }
+        if(prime == true)
+        {
+            printf("Yes\n");
+        }else
+        {
+            printf("No\n");
+        }
+    }
+    return 0;
+}
+
---
+++ PAT/group_tianti/L1-029.cpp
@@ -0,0 +1,22 @@
+/*
+*
+*      Filename: L1-029.cpp
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-05-06 01:10:09
+* Last Modified: 2017-05-06 01:15:38
+**/
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    int H;
+    scanf("%d",&H);
+    printf("%.1f\n",(H - 100)*0.9*2);
+    return 0;
+}
+
