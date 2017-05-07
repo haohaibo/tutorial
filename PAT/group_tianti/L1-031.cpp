ac15f48581b4db1a405ffa3b6e86ed7bb15b21a7
Haibo Hao <haohaibo031113@163.com>
2017-05-08 01:19:13 +0800

    PAT L1-031

---
+++ PAT/group_tianti/L1-031.cpp
@@ -0,0 +1,45 @@
+/*
+*
+*      Filename: L1-031.cpp
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-05-08 00:43:37
+* Last Modified: 2017-05-08 01:18:35
+**/
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    float H, W, S;
+    for(int i = 0; i < N; ++i)
+    {
+        scanf("%f",&H);
+        scanf("%f",&W);
+        W = W/2.0;
+        S = (H - 100)*0.9;  
+        if(fabs(S - W) < S*0.1) 
+        {
+            printf("You are wan mei!\n");
+        }else
+        {
+            if(W > S)
+            {
+                printf("You are tai pang le!\n");
+            }else
+            {
+                printf("You are tai shou le!\n");
+            }
+
+        }
+    }
+    return 0;
+}
