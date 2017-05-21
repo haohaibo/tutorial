e939e498ef77c5cc6477e50f0c650b4e6db1a226
Haibo Hao <haohaibo031113@163.com>
2017-05-21 11:04:39 +0800

    PAT L1-048

---
+++ PAT/group_tianti/L1-033.cpp
@@ -0,0 +1,23 @@
+/*
+*
+*      Filename: L1-033.cpp
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-05-21 10:08:02
+* Last Modified: 2017-05-21 10:11:17
+**/
+#include <iostream>
+#include <string.h>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
+
