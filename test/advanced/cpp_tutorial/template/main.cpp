e73103de06c2e6793126ccbc843a12ed0397a08e
Haibo Hao <haohaibo031113@163.com>
2017-06-01 11:37:14 +0800

    add C++ 类模板

--- test/advanced/cpp_tutorial/template/main.cpp
+++ test/advanced/cpp_tutorial/template/main.cpp
@@ -6,17 +6,29 @@
 *        Email : haohaibo@ncic.ac.cn
 *   Description: ---
 *        Create: 2017-06-01 10:48:32
-* Last Modified: 2017-06-01 10:59:24
+* Last Modified: 2017-06-01 11:36:48
 **/
-#include "method.h"
 #include <stdio.h>
 #include <iostream>
+#include <string>
+#include "method.h"
+#include "stack.h"
 
 int main()
 {
     float data1 = 0.3, data2 = 0.6;
     swap<float>(data1, data2);
     std::cout << "data1 = " << data1 << ", data2 = " << data2 << std::endl;
+
+    Stack<int> intStack;
+    intStack.push(2);
+    intStack.push(4);
+    intStack.push(1);
+
+    while(!intStack.isEmpty())
+    {
+        std::cout << "num: " << intStack.pop() << std::endl;
+    }
     return 0;
 }
 
---
+++ test/advanced/cpp_tutorial/template/stack.h
@@ -0,0 +1,27 @@
+/*
+*
+*      Filename: stack.h
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-06-01 11:11:52
+* Last Modified: 2017-06-01 11:11:52
+**/
+template <class T>
+class Stack
+{
+    public:
+        Stack();
+        ~Stack();
+        void push(T t);
+        T pop();
+        bool isEmpty();
+    private:
+        T *m_pT;
+        int m_maxSize;
+        int m_size;
+};
+
+#include "stack.cpp"
+
