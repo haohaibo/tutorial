35f8dbd200f4f9664e75b4ce4afddd36d8e5db93
Haibo Hao <haohaibo031113@163.com>
2017-07-05 00:25:39 +0800

    added cpp-primer unordered_set in set.cpp

--- test/advanced/cpp-primer/chapter11/test/set.cpp
+++ test/advanced/cpp-primer/chapter11/test/set.cpp
@@ -6,20 +6,22 @@
 *        Email : haohaibo@ncic.ac.cn
 *   Description: ---
 *        Create: 2017-07-04 23:36:16
-* Last Modified: 2017-07-04 23:43:02
+* Last Modified: 2017-07-05 00:24:40
 **/
 #include <iostream>
 #include <set>
+#include <unordered_set>
 
 int main(int argc, char* argv[])
 {
     std::set<int> s1;
     std::set<int , std::greater<int> > s2;
-
+    std::unordered_set<int> s3;
     for(int i = 1; i < 6; ++i)
     {
         s1.insert(i);
         s2.insert(i);
+        s3.insert(i);
     }
 
     std::set<int>::const_iterator it = s1.begin();
@@ -31,6 +33,12 @@ int main(int argc, char* argv[])
     for(; it != s2.end(); ++it)
     {
         std::cout << *it << std::endl;
+    }
+
+    std::unordered_set<int>::const_iterator uit = s3.begin();
+    for(; uit != s3.end(); ++uit)
+    {
+        std::cout << *uit << std::endl;
     }
     return 0;
 }
