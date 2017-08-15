e57b51716b82af51ace8216709aaa90db9cb7951
Haibo Hao <haohaibo031113@163.com>
2017-08-15 20:34:54 +0800

    add template constructor

--- test/advanced/cpp_tutorial/template/example/main.cpp
+++ test/advanced/cpp_tutorial/template/example/main.cpp
@@ -6,7 +6,7 @@
 *        Email : haohaibo@ncic.ac.cn
 *   Description: ---
 *        Create: 2017-08-15 20:10:16
-* Last Modified: 2017-08-15 20:17:09
+* Last Modified: 2017-08-15 20:34:17
 **/
 #include<iostream>
 #include<cstdio>
@@ -15,9 +15,20 @@
 
 struct input_tensor_fixture
 {
+    int data;
+    input_tensor_fixture()
+    {
+        data = 2;
+    }
+    input_tensor_fixture(int in_data):data(in_data)
+    {
+
+    }
+
     void run()
     {
         std::cout << "running input_tensor_fixture" << std::endl;
+        std::cout << "data = " << data << std::endl;
     }
 };
 
