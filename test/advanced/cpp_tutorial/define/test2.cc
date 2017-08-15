a6ebbb8011319d0bf079ce06d561a5075a7b6208
Haibo Hao <haohaibo031113@163.com>
2017-08-15 10:39:59 +0800

    add construct type

--- test/advanced/cpp_tutorial/define/test2.cc
+++ test/advanced/cpp_tutorial/define/test2.cc
@@ -25,11 +25,21 @@
     if(!(__VA_ARGS__)) \
 failed_abort(#__VA_ARGS__,__FILE__,__LINE__)
 
+/* Constructs type name from a struct */
+#define MIOPEN_DECLARE_OBJECT(name) \
+    struct name                     \
+    {                               \
+    };                              \
+    typedef struct name* name##_t;
 
 int main()
 {
     int n = 100;
     EXPECT(n == 100);
+    /* Create miopenTensorDescriptor_t type */
+    MIOPEN_DECLARE_OBJECT(miopenTensorDescriptor);
+
+    miopenTensorDescriptor_t t;
     return 0;
 }
 
