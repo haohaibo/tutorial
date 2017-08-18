bf35b259e0ba328d9709acfbf82f6ec2fd1c600c
HenryHao <haohaiboyongyuan@gmail.com>
2017-08-18 12:58:52 +0800

    add MPI_Scatter

--- MPI/MPI-tutorial/collective_communication.c
+++ MPI/MPI-tutorial/collective_communication.c
@@ -19,15 +19,26 @@ int main(int argc, char * argv[])
     float sendbuf[SIZE][SIZE] = {
         {1.0, 2.0, 3.0, 4.0},
         {5.0, 6.0, 7.0, 8.0},
-        {9,0, 10.0, 11.0, 12.0},
+        {9.0, 10.0, 11.0, 12.0},
         {13.0, 14.0, 15.0, 16.0}
-    }
-    float recvbuf[SIZE][SIZE];
+    };
+    float recvbuf[SIZE];
 
     MPI_Init(&argc, &argv); //开始MPI环境
-    MPI_Comm_size(MPI_COMM_WORLD, numtasks);
-    MPI_Comm_rank(MPI_COMM_WORLD, rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    if(numtasks == SIZE) {
+        source = 1;
+        sendcount = SIZE;
+        recvcount = SIZE;
+        MPI_Scatter(sendbuf, sendcount, MPI_FLOAT, recvbuf, recvcount,
+                MPI_FLOAT, source, MPI_COMM_WORLD);
+
+        printf("rank = %d Results: %f %f %f %f\n", rank, recvbuf[0], recvbuf[1], recvbuf[2], recvbuf[3]);
+    }else{
+        printf("Must specify %d processors. Terminating.\n",SIZE);
+    }
 
     MPI_Finalize(); //结束MPI环境
     return 0;
