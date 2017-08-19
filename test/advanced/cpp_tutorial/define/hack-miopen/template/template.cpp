fb01088aca638d8a55dcf8b95d4c3f40a9b2f570
Haibo Hao <haohaibo031113@163.com>
2017-08-19 02:42:50 -0700

    add simple template in namespace

---
+++ test/advanced/cpp_tutorial/define/hack-miopen/template/template.cpp
@@ -0,0 +1,40 @@
+/*
+*
+*      Filename: template.cpp
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-08-19 02:23:38
+* Last Modified: 2017-08-19 02:42:22
+**/
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+namespace ns{
+    
+    // class template
+    template <class T1, class T2>
+        class A{
+            T1 data1;
+            T2 data2;
+        };
+
+    // function template
+    template <class T>
+        T max(const T lhs, const T rhs){
+            return lhs > rhs ? lhs : rhs;
+        }
+}
+
+int main(int argc, char* argv[])
+{
+    
+    std::cout << ns::max<float>(4.2,9.5) << std::endl;
+    // 可以自动推导类型
+    //std::cout << ns::max(4.3,9.2) << std::endl;
+
+    return 0;
+}
+
