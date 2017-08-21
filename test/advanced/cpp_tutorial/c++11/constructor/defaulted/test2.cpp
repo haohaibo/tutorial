dec93ec7eeb9d3f31c262512b1f60c400f26375e
Haibo Hao <haohaibo031113@163.com>
2017-08-21 10:03:57 +0800

    add c++11 default

--- test/advanced/cpp_tutorial/c++11/constructor/defaulted/test2.cpp
+++ test/advanced/cpp_tutorial/c++11/constructor/defaulted/test2.cpp
@@ -6,7 +6,7 @@
  *        Email : haohaibo@ncic.ac.cn
  *   Description: ---
  *        Create: 2017-08-20 23:07:26
-* Last Modified: 2017-08-20 23:59:05
+* Last Modified: 2017-08-21 10:03:23
  **/
 #include <iostream>
 #include <cstdio>
@@ -25,6 +25,30 @@ namespace ns {
         private:
             int y;
     };
+
+    class P {
+        public:
+            // 手动定义虚构函数
+            virtual ~P(){};
+        private:
+            int p;
+    };
+    class Q : public P {
+        private:
+            int q;
+    };
+
+    class M {
+        public:
+            // 手动定义虚构函数
+            virtual ~M()= default;
+        private:
+            int p;
+    };
+    class N : public M {
+        private:
+            int n;
+    };
 }
 int main(int argc, char* argv[])
 {
@@ -34,13 +58,31 @@ int main(int argc, char* argv[])
     // 为类X生成一个析构函数，从而可以成功的销毁x
     // 指向的派生类对象中的基类子对象(即int型成员
     // 变量x)
-    
+
     // 这段代码存在内存泄露问题，当利用delete语句
     // 删除指向派生类对象的指针x时，系统调用的是基
     // 类的析构函数，而非派生类Y类的析构函数,因此
     // 编译器无法析构派生类的int型成员变量y
     ns::X* x = new ns::Y;
     delete x;
+
+    // 因此，一般情况下，我们需要将基类的析构函数
+    // 定义为虚函数，当利用delete语句删除指向派生
+    // 类对象的基类指针时，系统会调用相应的派生类
+    // 的析构函数(实现多态性)，从而避免内存泄露。
+    // 但是编译器隐式生成的析构函数都是非虚函数
+    // 需要手动为基类P定义析构函数
+
+    // 当利用delete语句删除指向派生类对象的基类指针
+    // p，系统会调用相应的派生类Y的析构函数(有编译器
+    // 隐式自动生成)以及基类P的析构函数，从而将派生类
+    // 对象完整的销毁
+    ns::P* p = new ns::Q;
+    delete p;
+
+    ns::M* m = new ns::N;
+    delete m;
+
     return 0;
 }
 
