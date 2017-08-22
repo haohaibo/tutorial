3dc14ba5ad34b2b4cbed9a788f9d424c6cc051bb
Haibo Hao <haohaibo031113@163.com>
2017-08-22 22:05:43 +0800

    add namespace enum

---
+++ test/advanced/cpp_tutorial/namespace/enum.cc
@@ -0,0 +1,31 @@
+/*
+*
+*      Filename: enum.cc
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-08-22 21:59:14
+* Last Modified: 2017-08-22 21:59:14
+**/
+#include <iostream>
+#include <cstdio>
+
+namespace ns {
+    enum eMat{
+        matA,
+        matB,
+        matC,
+        nMats
+    };
+}
+
+int main(int argc, char* argv[]){
+    std::cout << ns::matA << std::endl;
+    std::cout << ns::matB << std::endl;
+    std::cout << ns::matC << std::endl;
+    std::cout << ns::nMats << std::endl;
+
+    return 0;
+}
+
