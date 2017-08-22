0e16077cdbe4c920fb205c704de3bf36fade0d5a
Haibo Hao <haohaibo031113@163.com>
2017-08-22 22:04:34 +0800

    add std::vector.at()

---
+++ test/advanced/cpp_tutorial/vector/at.cpp
@@ -0,0 +1,32 @@
+/*
+*
+*      Filename: at.cpp
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-08-22 19:20:38
+* Last Modified: 2017-08-22 22:04:09
+**/
+#include <iostream>
+#include <vector>
+#include <cstdio>
+
+// ref: http://www.cplusplus.com/reference/vector/vector/at/
+int main(int argc, char* argv[]){
+    // 10 zero-initialized ints
+    std::vector<int> myvector(10);
+
+    // assign some value
+    for(unsigned i = 0; i < myvector.size(); i++){
+        myvector.at(i) = i;
+    }
+    
+    std::cout << "myvector contains:";
+    for(unsigned i = 0; i < myvector.size(); i++){
+        std::cout << " " << myvector.at(i);
+    }
+    std::cout << std::endl;
+    return 0;
+}
+
