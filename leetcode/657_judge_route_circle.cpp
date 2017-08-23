d1c7f8102700768d901db5ef5727e8247f59c087
Haibo Hao <haohaibo031113@163.com>
2017-08-23 23:14:56 +0800

    add leetcode 657

---
+++ leetcode/657_judge_route_circle.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int pos_x = 0,pos_y = 0;
+        for(size_t i=0; i < moves.length(); ++i){
+            if(moves[i] == 'U'){
+                ++pos_y;
+            }else if(moves[i] == 'R'){
+                ++pos_x;
+            }else if(moves[i] == 'D'){
+                --pos_y;
+            }else if(moves[i] == 'L'){
+                --pos_x;
+            }
+        }
+        if(pos_x == 0 && pos_y == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
