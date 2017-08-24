e76c4dfeac329ac648c15ea1b72833c817922781
Haibo Hao <haohaibo031113@163.com>
2017-08-24 15:07:20 +0800

    add 575. Distribute Candies

---
+++ leetcode/575_distribute_candies.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candies) {
+        set<int> kinds;
+        for(size_t i  = 0; i < candies.size(); ++i){
+            kinds.insert(candies[i]);
+        }
+        if(kinds.size() >= candies.size()/2){
+            return candies.size()/2;
+        }else
+        {
+            return kinds.size();
+        }
+    }
+};
