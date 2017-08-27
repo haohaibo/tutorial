2c6f803d91907384665650e2bf3fe81921121cfa
Haibo Hao <haohaibo031113@163.com>
2017-08-27 20:48:21 +0800

    add leetcode 53. Maximum Subarray

---
+++ leetcode/53_maximun_subarray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = nums[0];
+        int max = nums[0];
+        for(int i=1;i<nums.size();++i){
+            sum = sum < 0 ? nums[i] : sum + nums[i];
+            max = max < sum ? sum : max;
+        }
+        return max;
+    }
+};
