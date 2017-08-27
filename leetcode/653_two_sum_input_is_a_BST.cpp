19547d2067fb79fc3c9eebb1063aa057a51750d4
Haibo Hao <haohaibo031113@163.com>
2017-08-27 15:58:49 +0800

    add leetcode 653.Two Sum IV - Input is a BST

---
+++ leetcode/653_two_sum_input_is_a_BST.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        vector<int> data;
+        Inorder(root, data);
+        int temp;
+        for(int i=0;i<data.size();++i){
+            temp = k - data[i];
+            if(temp>=data[i]){
+                for(int j=i+1;j<data.size();++j){
+                    if(data[j] == temp){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    void Inorder(TreeNode* root, vector<int> &data){
+        if(root == NULL)
+            return;
+        
+        Inorder(root->left, data);
+        data.push_back(root->val);
+        Inorder(root->right, data);
+    }
+};
