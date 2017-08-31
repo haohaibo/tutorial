f4cf78f64b5c62fb5db2e1754999e1f26f0b8a52
Haibo Hao <haohaibo031113@163.com>
2017-08-31 15:03:41 +0800

    add leetcode 563. Binary Tree Tilt

---
+++ leetcode/563_binary_tree_tilt.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int findTilt(TreeNode* root) {
+        if(root == nullptr)
+            return 0;
+        return findTiltOfNode(root) + findTilt(root->left) + findTilt(root->right);
+    }
+    int findTiltOfNode(TreeNode* root){
+        if(root == nullptr)
+            return 0;
+        return abs(SumOfTree(root->left) - SumOfTree(root->right));
+    }
+    
+    int SumOfTree(TreeNode* root){
+        if(root == nullptr)
+            return 0;
+        return root->val + SumOfTree(root->left) + SumOfTree(root->right);
+    }
+};
