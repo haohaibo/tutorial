a2583a07593a0dbb0997f57f478304b5154c031a
Haibo Hao <haohaibo031113@163.com>
2017-09-03 12:26:22 +0800

    add c++ vector variable

--- test/advanced/cpp_tutorial/vector/var.cpp
+++ test/advanced/cpp_tutorial/vector/var.cpp
@@ -6,22 +6,52 @@
 *        Email : haohaibo@ncic.ac.cn
 *   Description: ---
 *        Create: 2017-09-03 10:40:09
-* Last Modified: 2017-09-03 10:44:37
+* Last Modified: 2017-09-03 12:26:07
 **/
 #include <iostream>
 #include <cstdio>
 #include <vector>
 
-int main(int argc, char* argv[]){
+namespace ns {
+
     std::vector<int> p = {1, 2, 3};
-    std::vector<int> (q) = {3, 4, 6};
+    //std::vector<int> (p);
+
+    namespace sub_ns {
+        std::vector<int> (p);
+    }
+
+    void func(std::vector<int> p){
+        for(auto ele : p){
+            std::cout << ele << std::endl;
+        }
+    }
+}
+
+int main(int argc, char* argv[]){
+    std::vector<int> (q);
+    q.push_back(4);
+    q.push_back(1);
     
-    for(auto ele : p){
+    for(auto ele : ns::p){
+        std::cout << ele << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    ns::sub_ns::p.push_back(0);
+    for(auto ele : ns::sub_ns::p){
         std::cout << ele << std::endl;
     }
     std::cout << std::endl;
     for(auto ele : q){
         std::cout << ele << std::endl;
     }
+
+    std::vector<int> p;
+    p.push_back(233);
+    ns::func(std::vector<int> (p));
+
+    return 0;
 }
 
