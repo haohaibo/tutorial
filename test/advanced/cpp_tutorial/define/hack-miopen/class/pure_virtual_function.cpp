32aef555bb651657656d691618055884660a5270
Haibo Hao <haohaibo031113@163.com>
2017-09-03 19:35:53 +0800

    add c++ class pure virtual function

---
+++ test/advanced/cpp_tutorial/define/hack-miopen/class/pure_virtual_function.cpp
@@ -0,0 +1,44 @@
+/*
+*
+*      Filename: pure_function.cpp
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-09-03 19:25:01
+* Last Modified: 2017-09-03 19:34:56
+**/
+#include <iostream>
+#include <cstdio>
+
+namespace ns {
+    class Base {
+        public :
+            virtual void Vfunc() = 0;
+            int x;
+    };
+    void Base::Vfunc(){
+        x = 3;
+        std::cout << x << std::endl;
+    }
+
+    class Derived : public Base {
+        public :
+            void Vfunc(){
+                Base::Vfunc();
+            }
+    };
+
+}
+int main(int argc, char* argv[]){
+
+    // invalid new-expression of abstract class type ‘ns::Base’
+    //ns::Base* pb = new ns::Base;
+    
+    ns::Base* pb = new ns::Derived;
+    pb->Vfunc();
+    delete pb;
+
+    return 0;
+}
+
