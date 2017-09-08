04aa94c7988d5050dab3de5ee84028976e1834ff
Haibo Hao <haohaibo031113@163.com>
2017-09-08 09:55:58 +0800

    add c++ getopt

---
+++ test/advanced/cpp_tutorial/getopt/getopt.cpp
@@ -0,0 +1,22 @@
+/*
+*
+*      Filename: getopt.cpp
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-08-22 16:16:36
+* Last Modified: 2017-08-22 16:31:46
+**/
+#include <iostream>
+#include <cstdio>
+// ref: https://www.ibm.com/developerworks/cn/aix/library/au-unix-getopt.html
+int main(int argc, char* argv[]){
+    std::cout << argv[0] << std::endl;
+    std::cout << argv[1] << std::endl;
+    std::cout << argv[2] << std::endl;
+    std::cout << argv[3] << std::endl;
+    
+    return 0;
+}
+
