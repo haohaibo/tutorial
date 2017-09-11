ea81c81360db469adbea40200adb255f7bc0b3a1
Haibo Hao <haohaibo031113@163.com>
2017-09-11 20:56:32 +0800

    add perl scalar, array

--- perl/variable.pl
+++ perl/variable.pl
@@ -1,7 +1,32 @@
-
+# Scalar Array Hash
+#
+# Scalar 
 my $animal = "camel";
 my $answer = 42;
 
 print $animal, "\n";
 print "The animal is $animal\n";
 print "The square of $answer is ", $answer * $answer, "\n";
+print; # prints contents of $_ by default
+
+# Array
+my @animals = ("camel", "llama", "owl");
+my @numbers = (23, 42, 69);
+my @mixed = ("camel", 42, 1.24);
+
+# 0 index
+print $animals[0], "\n";
+print $animals[2], "\n";
+
+# $# -- the last elements index in array 
+print $mixed[$#mixed],"\n";
+print @animals, "\n";
+print @animals[0,1], "\n";
+
+# sort and reverse are perl's built-in function
+my @sorted = sort @animals;
+my @backwards = reverse @numbers;
+
+print @sorted, "\n";
+print @backwards, "\n";
+
