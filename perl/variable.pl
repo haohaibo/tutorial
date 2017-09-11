eb85746f8d5c0dc766bf647d1d3c272bb627b6c4
Haibo Hao <haohaibo031113@163.com>
2017-09-11 21:32:37 +0800

    add perl hash

--- perl/variable.pl
+++ perl/variable.pl
@@ -9,6 +9,9 @@ print "The animal is $animal\n";
 print "The square of $answer is ", $answer * $answer, "\n";
 print; # prints contents of $_ by default
 
+my $result = "This is " + "$answer";
+print "$result\n";
+
 # Array
 my @animals = ("camel", "llama", "owl");
 my @numbers = (23, 42, 69);
@@ -30,3 +33,37 @@ my @backwards = reverse @numbers;
 print @sorted, "\n";
 print @backwards, "\n";
 
+# consider context
+if(@animals < 5){
+    print "[OK] # of animals < 5\n";
+}
+
+# Hash
+#my %fruit_color = ("apple", "red", "banana", "yellow");
+my %fruit_color = (
+    apple => "red",
+    banana => "yellow",
+);
+
+print %fruit_color, "\n";
+print $fruit_color{"apple"},"\n";
+
+#
+my $variables = {
+    scalar => {
+        description => "single item",
+        sigil => '$',
+    },
+    array => {
+        description => "ordered list of items",
+        sigil => '@',
+    },
+    hash => {
+        description => "key/value pairs",
+        sigil => '%',
+    },
+};
+
+print "$variables->{'scalar'}->{'sigil'}\n";
+print "$variables->{'array'}->{'sigil'}\n";
+print "$variables->{'hash'}->{'sigil'}\n";
