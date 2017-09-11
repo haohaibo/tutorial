f47de86400691bf3647641c3d96d2a2294a43375
Haibo Hao <haohaibo031113@163.com>
2017-09-11 20:18:36 +0800

    add perl tutorial

---
+++ perl/README.md
@@ -0,0 +1,3 @@
+# perl tutorial
+
+### be familiar with perl quickly
---
+++ perl/variable.pl
@@ -0,0 +1,7 @@
+
+my $animal = "camel";
+my $answer = 42;
+
+print $animal, "\n";
+print "The animal is $animal\n";
+print "The square of $answer is ", $answer * $answer, "\n";
