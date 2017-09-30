19f47bf9e2ddca5f4cbdcba5501f4d65425f164b
Haibo Hao <haohaibo031113@163.com>
2017-09-30 17:14:34 +0800

    add gcc编译的四个阶段

---
+++ test/advanced/cpp_tutorial/define/hack-miopen/compile/README.md
@@ -0,0 +1,2 @@
+# GCC编译的四个阶段
+* reference: [GCC编译的四个阶段](http://blog.csdn.net/bupt073114/article/details/37049829)
---
+++ test/advanced/cpp_tutorial/define/hack-miopen/compile/helloworld.c
@@ -0,0 +1,17 @@
+/*
+*
+*      Filename: helloworld.c
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-09-30 15:58:12
+* Last Modified: 2017-09-30 15:58:12
+**/
+#include <stdio.h>
+
+int main(){
+    printf("hello world!\n");
+    return 0;
+}
+
