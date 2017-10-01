c913335f412dff8aedb4494ebe51108c59df4eeb
Haibo Hao <haohaibo031113@163.com>
2017-10-01 19:06:08 +0800

    refactor cpp-primer 1.2 理解std::endl操纵符

--- test/advanced/cpp-primer/chapter1/1.2/proj1_2_2.cc
+++ test/advanced/cpp-primer/chapter1/1.2/proj1_2_2.cc
@@ -18,7 +18,7 @@ int main()
     std::cout << "test";
     // 在C++中，一个表达式产生一个运算结果，它由一个或多个运算对象和(通常)一个运算符组成
     // << 为输出运算符
-    // endl是一个被称为操纵符的特殊值
+    // endl是一个被称为操纵符(manipulator)的特殊值
     // 写入endl的效果是结束当前行，并将与设备关联的缓冲区(buffer)中的内容刷到设备中
     std::cout << std::endl;
     return 0;
