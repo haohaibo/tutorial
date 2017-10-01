5d8a71c12555a8566e1c901a569a3a9fd9da6d76
Haibo Hao <haohaibo031113@163.com>
2017-10-01 19:04:37 +0800

    add cpp-primer 1.2理解std::endl操纵符

--- test/advanced/cpp-primer/chapter1/1.2/proj1_2_2.cc
+++ test/advanced/cpp-primer/chapter1/1.2/proj1_2_2.cc
@@ -12,5 +12,14 @@ int main()
     std::cin >> v1 >> v2;
     std::cout << "The sum of " << v1 << " and "
               << v2 << " is " << v1 + v2 << std::endl;
+    std::cerr << "cerr: standard error" << std::endl;
+    std::clog << "clog: log info" << std::endl;
+
+    std::cout << "test";
+    // 在C++中，一个表达式产生一个运算结果，它由一个或多个运算对象和(通常)一个运算符组成
+    // << 为输出运算符
+    // endl是一个被称为操纵符的特殊值
+    // 写入endl的效果是结束当前行，并将与设备关联的缓冲区(buffer)中的内容刷到设备中
+    std::cout << std::endl;
     return 0;
 }
