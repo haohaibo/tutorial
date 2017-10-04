84e8b6648b95692dc2b231acaa0f9fcef0877122
Haibo Hao <haohaibo031113@163.com>
2017-10-04 22:56:28 +0800

    add leaning-cmake

---
+++ cmake/tutorial/backup/main.c
@@ -0,0 +1,18 @@
+/*
+*
+*      Filename: main.c
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-10-04 11:42:10
+* Last Modified: 2017-10-04 11:42:10
+**/
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world cmake\n");
+    return 0;
+}
+
