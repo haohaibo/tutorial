d7b139e0284c86de603e2b55eeb18d8ba7de0801
Haibo Hao <haohaibo031113@163.com>
2017-10-28 00:51:36 +0800

    add hiho 173 dp

---
+++ hiho/hiho_173.cpp
@@ -0,0 +1,47 @@
+/*
+ *
+ *      Filename: hiho_173.cpp
+ *
+ *        Author: Haibo Hao
+ *        Email : haohaibo@ncic.ac.cn
+ *   Description: ---
+ *        Create: 2017-10-28 00:49:45
+* Last Modified: 2017-10-28 00:50:35
+ **/
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int s[n],prefixsum[n];
+    int psum=0;
+
+
+    for(int i=0;i<n;++i){
+        cin>>s[i];
+        psum+=s[i];
+        prefixsum[i]=psum;
+
+    }
+    int f[n][n];
+
+    for(int i=0;i<n;++i){
+        f[i][i]=s[i];
+    }
+
+    for(int l=2;l<=n;++l){
+        for(int i=0;i<n-l+1;++i){
+            int j=i+l-1;
+
+            f[i][j]=max(prefixsum[j]-prefixsum[i-1]-f[i+1][j],
+                    prefixsum[j]-prefixsum[i-1]-f[i][j-1]);
+        }
+    }
+
+    cout<<f[0][n-1]<<endl;
+
+}
+
---
+++ test/advanced/cpp_tutorial/static/test1/a.cpp
@@ -0,0 +1,18 @@
+/*
+*
+*      Filename: a.cpp
+*
+*        Author: Haibo Hao
+*        Email : haohaibo@ncic.ac.cn
+*   Description: ---
+*        Create: 2017-09-22 14:49:40
+* Last Modified: 2017-09-22 14:53:41
+**/
+#include<iostream>
+#include<cstdio>
+
+char a = 'A'; // global variable
+void msg(){
+    std::cout << "hello world" << std::endl;
+}
+
