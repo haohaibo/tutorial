7287a87433e9007b42b78594e29cb1fe839baf10
Haibo Hao <haohaibo031113@163.com>
2017-10-28 22:19:33 +0800

    add leetcode 268 missing number

---
+++ leetcode/268_missing_number.cpp
@@ -0,0 +1,22 @@
+/*
+ *
+ *      Filename: 268_missing_number.cpp
+ *
+ *        Author: Haibo Hao
+ *        Email : haohaibo@ncic.ac.cn
+ *   Description: ---
+ *        Create: 2017-10-28 22:18:59
+* Last Modified: 2017-10-28 22:19:18
+ **/
+class Solution {
+    public:
+        int missingNumber(vector<int>& nums) {
+            int sum=0;
+            for(int i=0;i<nums.size();++i){
+                sum+=nums[i];
+            }
+            int n=nums.size();
+            return (n*(n+1)/2) - sum;
+        }
+};
+
