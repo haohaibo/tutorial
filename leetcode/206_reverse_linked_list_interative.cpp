0d4d3191c04a4e873b4b5386f59c811df69413e4
Haibo Hao <haohaibo031113@163.com>
2017-11-13 12:07:30 +0800

    add leetcode 206 reverse linked list

---
+++ leetcode/206_reverse_linked_list_interative.cpp
@@ -0,0 +1,34 @@
+/*
+ *
+ *      Filename: 206_reverse_linked_list_interative.cpp
+ *
+ *        Author: Haibo Hao
+ *        Email : haohaibo@ncic.ac.cn
+ *   Description: ---
+ *        Create: 2017-11-13 11:12:52
+* Last Modified: 2017-11-13 11:15:24
+ **/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+    public:
+        ListNode* reverseList(ListNode* head) {
+            ListNode* pre=NULL;
+            ListNode* cur=head;
+            ListNode* next;
+            while(cur!=NULL){
+                next=cur->next;
+                cur->next=pre;
+                pre=cur;
+                cur=next;
+            }
+            return pre;
+        }
+};
+
