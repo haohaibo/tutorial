bc777e0783ed17b0a17f8eeb10d37032601924f1
Haibo Hao <haohaibo031113@163.com>
2018-03-29 10:11:14 +0800

    add new Makefile style

--- make/test/test.c
+++ make/test/test.c
@@ -6,9 +6,17 @@
 #else
 #define MAX_THREAD 10
 #endif
+static const int AUX=2;
+int sum(int a, int b, int c=AUX){
+    return a + b;
+}
 
 int main(int argc, char* argv[]){
     printf("max "
             "thread = %d\n", MAX_THREAD);
+    int mysum, a, b;
+    a = 9, b = 2;
+    mysum = sum(a, b);
+    printf("%d + %d = %d\n",a, b, mysum);
     return 0;
 }
