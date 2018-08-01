0feb3fd2bb0490f1a04461f8616bc4eaa38eab0b
Haibo Hao <haohaibo01.gmail.com>
2018-08-01 01:48:11 -0700

    :lollipop: command make VPATH and vpath

---
+++ make/vpath/src/foo.c
@@ -0,0 +1,18 @@
+/*
+*
+*      Filename: foo.c
+*
+*        Author: Haibo Hao
+*        Email : hhao@nvidia.com
+*   Description: ---
+*        Create: 2018-07-31 18:53:53
+* Last Modified: 2018-07-31 18:53:53
+**/
+#include <iostream>
+#include <cstdio>
+
+int main(){
+    std::cout << "foo" << std::endl;
+    return 0;
+}
+
