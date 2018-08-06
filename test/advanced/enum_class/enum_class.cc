9feb86578074a5224871b75dc97d8d8c35f8206c
Haibo Hao <haohaibo01.gmail.com>
2018-08-06 08:25:20 -0700

    command enum class

---
+++ test/advanced/enum_class/enum_class.cc
@@ -0,0 +1,30 @@
+/*
+*
+*      Filename: enum_class.cc
+*
+*        Author: Haibo Hao
+*        Email : haohaibo01@gmail.com
+*   Description: ---
+*        Create: 2018-08-06 08:08:45
+* Last Modified: 2018-08-06 08:08:45
+**/
+#include <iostream>
+#include <cstdio>
+
+enum Direction {TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT};
+
+//enum WindowsCorner {TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT};
+
+enum class Color {red, green, blue};
+
+
+int main(){
+
+    //enum Direction dir = 1;
+    //std::cout << dir << std::endl;
+
+    auto green = false;
+    std::cout << green << std::endl;
+
+    return 0;
+}
