84c9b7b7fbf79f8b075d76c3da07564dbc921b1b
Haibo Hao <haohaibo01.gmail.com>
2018-08-07 20:07:22 -0700

    :coffee: understand extern "C", func() const

---
+++ test/advanced/overloading_const_func/const_overload.cc
@@ -0,0 +1,36 @@
+/*
+*
+*      Filename: const_overload.cc
+*
+*        Author: Haibo Hao
+*        Email : haohaibo01@gmail.com
+*   Description: ---
+*        Create: 2018-08-07 03:50:48
+* Last Modified: 2018-08-07 03:50:48
+**/
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+class Test{
+    protected:
+        int x;
+    public:
+        Test(int i):x(i){}
+        void func() const{
+            cout << "func() const called" << endl;
+        }
+        void func(){
+            cout << "func() called" << endl;
+        }
+};
+
+int main(){
+    Test t1(10);
+    const Test t2(20);
+    t1.func();
+    t2.func();
+
+    return 0;
+}
+
