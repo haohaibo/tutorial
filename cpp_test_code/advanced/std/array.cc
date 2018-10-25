c489b659308051d8f4c0a3f8b88ec6d61c46c6fd
Haibo Hao <haohaibo01@gmail.com>
2018-10-25 21:35:50 +0800

    understand c++ function argument passing

---
+++ cpp_test_code/advanced/std/array.cc
@@ -0,0 +1,13 @@
+#include <array>
+#include <iostream>
+
+int main()
+{
+    std::array<int, 3> a1{ {1, 2, 3} };
+    for(int i = 0; i < 3; ++i)
+    {
+        std::cout << a1[i] << " ";
+    }
+    std::cout <<"\n";
+    return 0;
+}
