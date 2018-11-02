f9c55c12dd8809bc22c6aeb3ea1c28cf2d007329
Haibo Hao <haohaibo01@gmail.com>
2018-11-02 13:25:12 +0800

    add c++ template example

---
+++ cpp_test_code/advanced/template/test.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+template < bool isA, bool isB>
+void func(int a, int b, int c)
+{
+    if(isA)
+        std::cout << "isA" << std::endl;
+    else
+        std::cout << "not A" << std::endl;
+}
+int main()
+{
+    int a = 1, b = 2, c = 2;
+    bool isA;
+    std::cin >> isA;
+    if(isA)
+        func<true, false>(a, b, c);
+    else
+        func<false, false>(a, b, c);
+    
+}
