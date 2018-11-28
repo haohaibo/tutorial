0cacf7ff2e37d7ed82471b2d53308f195f9299bd
Haibo Hao <haohaibo01.gmail.com>
2018-11-27 18:19:12 -0800

    have a command of linux patch tech

---
+++ linux/patch/hello.c
@@ -0,0 +1,17 @@
+/*
+*
+*      Filename: hello.c
+*
+*        Author: Haibo Hao
+*        Email : haohaibo01@gmail.com
+*   Description: ---
+*        Create: 2018-11-27 18:10:04
+* Last Modified: 2018-11-27 18:10:04
+**/
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world\n");
+}
+
---
+++ linux/patch/hello_new.c
@@ -0,0 +1,18 @@
+/*
+*
+*      Filename: hello.c
+*
+*        Author: Haibo Hao
+*        Email : haohaibo01@gmail.com
+*   Description: ---
+*        Create: 2018-11-27 18:10:04
+* Last Modified: 2018-11-27 18:10:04
+**/
+#include <stdio.h>
+
+int main(int argc, char* argv)
+{
+    printf("Hello world\n");
+    return 0;
+}
+
