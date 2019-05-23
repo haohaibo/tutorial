df132d74920d22a08c15bb963593d238d807974a
Haibo Hao <hhao@nvidia.com>
2019-05-24 01:00:49 +0800

    Create share_ptr_trap.cc

---
+++ C++/cpp_test_code/advanced/share_ptr_trap.cc
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<memory>
+
+using namespace std;
+
+int main()
+{
+   
+   shared_ptr<int> p(new int{8});
+   	
+   shared_ptr<int>& f = p;
+   cout << "xxxxxx  " << *f.get() << endl;
+   
+
+   int* q;
+   {
+	shared_ptr<int> shared(new int{90});
+	cout << shared.use_count() << endl;
+	
+	//f = shared;
+	//cout << "2333  " << *f.get() << endl;
+	
+	q = shared.get();
+	cout << "in *q = " << *q << endl;
+	cout << "in q = " << q << endl;
+	
+	
+   }
+   
+   cout << "\n\nout = *q = " << *q << endl;
+   cout << "out q = " << q << endl;
+   
+   cout << "sssss  " << *f.get() << endl;
+   
+   
+
+    return 0;
+} 
